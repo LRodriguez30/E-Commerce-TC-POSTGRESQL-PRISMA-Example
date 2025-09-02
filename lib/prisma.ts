bc7589a0ba39cdec7a361fafd56a7d3c40ae25d7
LRodriguez30 <lestertenorio30@gmail.com>
2025-09-02 17:19:42 -0600

    Prisma Connection

---
+++ lib/prisma.ts
@@ -0,0 +1,21 @@
+// src/lib/prisma.js
+import { PrismaClient } from "@prisma/client";
+
+declare global {
+  // Agregamos la propiedad 'prisma' al objeto global
+  // eslint-disable-next-line no-var
+  var prisma: PrismaClient | undefined;
+}
+
+let prisma: PrismaClient;
+
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient();
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+  prisma = global.prisma;
+}
+
+export default prisma;
